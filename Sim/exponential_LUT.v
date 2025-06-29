a05174c74bf4c67ab21a6f4dd8c8b0045b60f3a4
Gerónimo <62406795+gzadkiel@users.noreply.github.com>
2025-06-29 20:15:51 -0300

    Update exponential_LUT.v

--- Sim/exponential_LUT.v
+++ Sim/exponential_LUT.v
@@ -1,5 +1,5 @@
 module exponential_LUT #(
-    parameter P  =     , //! Number of data blocks
+    parameter P  = 1024, //! Number of data blocks
     parameter NP = 1024, //! Number of data samples per block
 ) (
     input           clock,                //! System clock     
@@ -17,69 +17,37 @@ module exponential_LUT #(
 // m  : index for each sample on the data block (0, 1, 2, ... , Np - 1)
 // p  : index for each data block (0, 1, 2, ... , P - 1)
 
-localparam INDX_WIDTH = P*NP;
+localparam INDX_P_WIDTH  = $clog(P);
+localparam INDX_NP_WIDTH = $clog(NP);
+localparam NB_EXP_COEFFS = ;
 
-reg [9              : 0] exp_real_part;
-reg [9              : 0] exp_imag_part;
-reg [INDX_WIDTH - 1 : 0] r_index;
-reg [9              : 0] re_LUT16   [16*P   - 1 : 0];
-reg [9              : 0] im_LUT16   [16*P   - 1 : 0];
-reg [9              : 0] re_LUT32   [32*P   - 1 : 0];
-reg [9              : 0] im_LUT32   [32*P   - 1 : 0];
-reg [9              : 0] re_LUT64   [64*P   - 1 : 0];
-reg [9              : 0] im_LUT64   [64*P   - 1 : 0];
-reg [9              : 0] re_LUT128  [128*P  - 1 : 0];
-reg [9              : 0] im_LUT128  [128*P  - 1 : 0];
-reg [9              : 0] re_LUT256  [256*P  - 1 : 0];
-reg [9              : 0] im_LUT256  [256*P  - 1 : 0];
-reg [9              : 0] re_LUT512  [512*P  - 1 : 0];
-reg [9              : 0] im_LUT512  [512*P  - 1 : 0];
-reg [9              : 0] re_LUT1024 [1024*P - 1 : 0];
-reg [9              : 0] im_LUT1024 [1024*P - 1 : 0];
+reg [NB_EXP_COEFFS     : 0] exp_real_part;
+reg [NB_EXP_COEFFS     : 0] exp_imag_part;
+reg [INDX_P_WIDTH  - 1 : 0] r_P_index;
+reg [INDX_NP_WIDTH - 1 : 0] r_NP_index;
+reg [NB_EXP_COEFFS     : 0] re_LUT1024 [1024*P - 1 : 0];
+reg [NB_EXP_COEFFS     : 0] im_LUT1024 [1024*P - 1 : 0];
 
-always @(posedge clock) begin
+always @(posedge clock) begin: LUT_Index_Management
     if (i_reset) begin
-        r_index <= {INDX_WIDTH{1'b0}};
+        r_P_index  <= {INDX_P_WIDTH{1'b0}};
+        r_NP_index <= {INDX_NP_WIDTH{1'b0}};
     end
     else if (i_enable == 1'b1 && i_m_axis_data_tvalid == 1'b1) begin
-        r_index <= r_index + 1'b1;
-        if (r_index == INDX_WIDTH - 1) begin
-            r_index <= {INDX_WIDTH{1'b0}};
+        // Increase NP index on each clock cycle
+        r_NP_index <= r_NP_index + 1'b1;
+        if (r_NP_index == NP - 1) begin
+            // Once NP samples, reset NP index to 0 and increase P index to start the next samples block
+            r_NP_index <= {INDX_NP_WIDTH{1'b0}};
+            r_P_index  <= r_P_index + 1'b1;
+            if (r_P_index == P - 1) begin
+                // Once P blocks, reset both indexes
+                r_P_index  <= {INDX_P_WIDTH{1'b0}};
+                r_NP_index <= {INDX_NP_WIDTH{1'b0}};
+            end
         end
-        case (i_NFFT_sel)
-            3'b000 : begin // 16
-                exp_real_part <= re_LUT16[r_index];
-                exp_imag_part <= im_LUT16[r_index];
-            end 
-            3'b001 : begin // 32
-                exp_real_part <= re_LUT32[r_index];
-                exp_imag_part <= im_LUT32[r_index];
-            end
-            3'b010 : begin // 64
-                exp_real_part <= re_LUT64[r_index];
-                exp_imag_part <= im_LUT64[r_index];
-            end
-            3'b011 : begin // 128
-                exp_real_part <= re_LUT128[r_index];
-                exp_imag_part <= im_LUT128[r_index];
-            end 
-            3'b100 : begin // 256
-                exp_real_part <= re_LUT256[r_index];
-                exp_imag_part <= im_LUT256[r_index];
-            end
-            3'b101 : begin // 512
-                exp_real_part <= re_LUT512[r_index];
-                exp_imag_part <= im_LUT512[r_index];
-            end
-            3'b110 : begin // 1024
-                exp_real_part <= re_LUT1024[r_index];
-                exp_imag_part <= im_LUT1024[r_index];
-            end
-            default: begin // 1024
-                exp_real_part <= re_LUT1024[r_index];
-                exp_imag_part <= im_LUT1024[r_index];
-            end
-        endcase
+        exp_real_part <= re_LUT1024[r_P_index][r_NP_index];
+        exp_imag_part <= im_LUT1024[r_P_index][r_NP_index];
     end
     else begin
         r_index <= r_index;
@@ -90,15 +58,32 @@ assign o_exp_sample_real = exp_real_part;
 assign o_exp_sample_imag = exp_imag_part;
 
 // LUTs
-// NP = 16
-assign re_LUT16[0] = 10'b0000000000; assign im_LUT16[0] = 10'b0000000000;
-assign re_LUT16[1] = 10'b0000000000; assign im_LUT16[1] = 10'b0000000000;
-assign re_LUT16[2] = 10'b0000000000; assign im_LUT16[2] = 10'b0000000000;
-assign re_LUT16[3] = 10'b0000000000; assign im_LUT16[3] = 10'b0000000000;
-assign re_LUT16[4] = 10'b0000000000; assign im_LUT16[4] = 10'b0000000000;
-assign re_LUT16[5] = 10'b0000000000; assign im_LUT16[5] = 10'b0000000000;
- 
-
-
+// Row P = 0 - LUT16[0][NPi]
+assign re_LUT16[0   ][0   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][0   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][1   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][1   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][3   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][3   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][4   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][4   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][5   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][5   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][6   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][6   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][7   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][7   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][8   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][8   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][9   ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][9   ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][10  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][10  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][11  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][11  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][12  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][12  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][13  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][13  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][14  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][14  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][15  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][15  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][16  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][16  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][17  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][17  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][18  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][18  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][19  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][19  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][20  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][20  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][21  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][21  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][22  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][22  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][23  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][23  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][24  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][24  ] = NB_EXP_COEFFS'b;
+assign re_LUT16[0   ][25  ] = NB_EXP_COEFFS'b; assign im_LUT16[0   ][25  ] = NB_EXP_COEFFS'b;
+// .......................
 
 endmodule
